1b01ad3c7b9dd819d829067052cf3a2a66903a08
aca333 <1akselcosta@gmail.com>
2024-05-03 05:37:31 +0300

    Update screenshot.ts

--- api/screenshot.ts
+++ api/screenshot.ts
@@ -1,52 +1,10 @@
-
 const chrome = require('@sparticuz/chromium')
 const puppeteer = require('puppeteer-core')
 
-// const getAbsoluteURL = (hash: string, path?: string) => {
-//   if (!process.env.NODE_ENV) {
-//     return `http://localhost:3000/${hash}`
-//   }
-  
-//   return `https://image.w.kodadot.xyz/ipfs/${path}/${hash}`
-// }
-
-// type ScreenshotRequest = {
-// 	url: string
-// 	settings?: Settings
-// }
-
-// export type Settings = {
-// 	delay?: number;
-// 	width?: number;
-// 	height?: number;
-// }
-
-
-const performCanvasCapture = async (page: any, canvasSelector: string) => {
-  try {
-    // get the base64 image from the CANVAS targetted
-    const base64 = await page.$eval(canvasSelector, el => {
-      if (!el || el.tagName !== "CANVAS") return null
-      return el.toDataURL()
-    })
-    if (!base64) throw new Error("No canvas found")
-    // remove the base64 mimetype at the beginning of the string
-    const pureBase64 = base64.replace(/^data:image\/png;base64,/, "")
-    return Buffer.from(pureBase64, "base64")
-  } catch (err) {
-    return null
-  }
-}
-
-export default async (req: any, res: any) => {
-  let {
-    // query: { hash, path, resolution },
-    body,
-    method
-  } = req
+export default async (req, res) => {
+  let { method } = req
 
   if (method !== 'POST') {
-    // CORS https://vercel.com/guides/how-to-enable-cors
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
@@ -57,15 +15,9 @@ export default async (req: any, res: any) => {
     return res.status(200).end()
   }
 
-  if (!body) return res.status(400).end(`No body provided`)
-
-  if (typeof body === 'object' && !body.url) return res.status(400).end(`No url provided`)
-
-  const isProd = process.env.NODE_ENV === 'production'
-
   let browser
 
-  if (isProd) {
+  try {
     browser = await puppeteer.launch({
       args: chrome.args,
       defaultViewport: chrome.defaultViewport,
@@ -73,48 +25,17 @@ export default async (req: any, res: any) => {
       headless: 'new',
       ignoreHTTPSErrors: true
     })
-  } else {
-    browser = await puppeteer.launch({
-      headless: 'new',
-      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    })
+
+    const page = await browser.newPage()
+
+    await page.goto('https://www.google.com');
+
+    await browser.close()
+
+    res.status(200).send('Successfully navigated to Google homepage')
+  } catch (error) {
+    if (browser) await browser.close()
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-
-  const page = await browser.newPage()
-
-  await page.setViewport({ width: 600, height: 600 })
-
-  // const url = getAbsoluteURL(`?hash=${hash}`, path)
-  const url = body.url
-
-  console.log('url', url)
-
-  await page.goto(url);
-
-  const selector = 'canvas';
-
-  await page.waitForSelector(selector);
-
-  const element = await performCanvasCapture(page, selector) // const element = page.$(selector)
-
-  // const data = await page.screenshot({
-  //   type: 'png'
-  // })
-
-  const data = element
-
-  await browser.close()
-  // Set the s-maxage property which caches the images then on the Vercel edge
-  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
-  res.setHeader('Content-Type', 'image/png')
-  // CORS
-  // res.setHeader('Access-Control-Allow-Headers', '*')
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  res.end(data)
 }

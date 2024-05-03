ab491aa2f8742410c000265d41a4a5d7ea47a78f
aca333 <1akselcosta@gmail.com>
2024-05-03 05:42:47 +0300

    Update screenshot.ts

--- api/screenshot.ts
+++ api/screenshot.ts
@@ -18,11 +18,11 @@ export default async (req, res) => {
 
       const page = await browser.newPage()
 
-      await page.goto('https://www.google.com');
+      await page.goto('https://twitter.com/realPengLoo');
 
       await browser.close()
 
-      res.status(200).send('Successfully navigated to Google homepage')
+      res.status(200).send('Successfully navigated to Twitter profile')
     } catch (error) {
       if (browser) await browser.close()
       console.error('Error:', error)
